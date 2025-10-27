3bf7af4ae2318ba2fe88c9d82d3e422423cc54f7
SamFranks4 <samfranks2007@gmail.com>
2025-10-27 16:09:50 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scrolling for sidebar links
+document.querySelectorAll('.sidebar nav a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const target = document.querySelector(link.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
